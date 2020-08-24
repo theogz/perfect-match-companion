3b7fa5dc4ec9a41807e3e1db3e05b32389460892
Theo Gantzer <11852762+theogz@users.noreply.github.com>
2020-08-23 23:22:51 -0700

    :construction: help

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,6 @@ void main() {
   runApp(MyApp());
 }
 
-var blocks = List.filled(25, Helpers.individualBlock);
-var fruits = Helpers.fruitList
-    .map<Widget>((fruit) => Helpers.emojiButton(fruit))
-    .toList();
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,34 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: title,
       theme: new ThemeData(scaffoldBackgroundColor: Colors.white),
-      home: Column(
-        // 5x5 matrix of potential fruit squares.
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.black),
-            child: GridView(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 2,
-                mainAxisSpacing: 2,
-                crossAxisCount: 5,
-              ),
-              children: blocks,
-            ),
-          ),
-          // List of fruits.
-          Container(
-              decoration: BoxDecoration(color: Colors.white),
-              child: GridView(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 2,
-                    mainAxisSpacing: 2,
-                    crossAxisCount: 6,
-                  ),
-                  children: fruits))
-        ],
-      ),
+      home: new Helpers.HomePage()
     );
   }
 }
