e92b5889be1081a5fd5f3f4a8f562b46b01c1977
Theo Gantzer <11852762+theogz@users.noreply.github.com>
2020-08-26 18:28:42 -0700

    :shirt: and 4x4 grid

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,59 +11,63 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String selectedEmoji = '';
-  List grid = List.filled(25, '');
+  List grid = List.filled(16, '');
 
-    void _changeSelectedEmoji(emoji) {
-        setState(() {
-            selectedEmoji = emoji;
-        });
-    }
+  void _changeSelectedEmoji(emoji) {
+    setState(() {
+      selectedEmoji = emoji;
+    });
+  }
 
-    void _updateArray(emoji, pos) {
-        setState(() {
-            grid[pos] = emoji;
-        });
-    }
+  void _updateArray(emoji, pos) {
+    setState(() {
+      grid[pos] = emoji;
+    });
+  }
 
-    void _clearArray() {
-        setState(() {
-            grid = List.filled(25, '');
-        });
-    }
+  void _clearArray() {
+    setState(() {
+      grid = List.filled(16, '');
+    });
+  }
 
-    generateGrid(tilesArray) {
-        return tilesArray.asMap().entries.map<Widget>((entry) => new GestureDetector(
+  generateGrid(tilesArray) {
+    return tilesArray
+        .asMap()
+        .entries
+        .map<Widget>(
+          (entry) => new GestureDetector(
             onTap: () {
-                _updateArray(this.selectedEmoji, entry.key);
-                return _changeSelectedEmoji('');
+              _updateArray(this.selectedEmoji, entry.key);
+              return _changeSelectedEmoji('');
             },
             child: Container(
-                color: Colors.white,
-                child: Material(
-                    child: Center(
-                        child: Text(entry.value, style: TextStyle(fontSize: 25))
-                    ),
-                ),
+              color: Colors.white,
+              child: Material(
+                child: Center(
+                    child: Text(entry.value, style: TextStyle(fontSize: 25))),
+              ),
             ),
-        ),
-        ).toList();
-    }
+          ),
+        )
+        .toList();
+  }
 
-    selectEmoji(emoji) {
-        return FlatButton(
-            color: Colors.white,
-            textColor: Colors.black,
-            disabledColor: Colors.white,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-                print(emoji);
-                _changeSelectedEmoji(emoji);
-            },
-            child: Text(emoji, style: TextStyle(fontSize: 35)),
-        );
-    }
+  selectEmoji(emoji) {
+    return FlatButton(
+      color: Colors.white,
+      textColor: Colors.black,
+      disabledColor: Colors.white,
+      disabledTextColor: Colors.black,
+      padding: EdgeInsets.all(8.0),
+      splashColor: Colors.blueAccent,
+      onPressed: () {
+        print(emoji);
+        _changeSelectedEmoji(emoji);
+      },
+      child: Text(emoji, style: TextStyle(fontSize: 35)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,14 +77,14 @@ class _HomePageState extends State<HomePage> {
       children: <Widget>[
         // 5x5 matrix of potential fruit squares.
         Container(
-            decoration: BoxDecoration(color: Colors.black),
-            child: GridView(
-                shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 2,
-                    mainAxisSpacing: 2,
-                    crossAxisCount: 5,
-                ),
+          decoration: BoxDecoration(color: Colors.black),
+          child: GridView(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2,
+              crossAxisCount: 5,
+            ),
             children: tiles,
           ),
         ),
@@ -104,18 +108,15 @@ class _HomePageState extends State<HomePage> {
         ),
         // Reset grid functionality.
         ConstrainedBox(
-            constraints: BoxConstraints(
-                minWidth: 500,
-                minHeight: 160
+          constraints: BoxConstraints(minWidth: 500, minHeight: 160),
+          child: Container(
+            color: Colors.white,
+            child: FlatButton(
+              color: Colors.white,
+              child: Text('RESET GRID', style: TextStyle(color: Colors.black)),
+              onPressed: () => _clearArray(),
             ),
-            child: Container(
-                color: Colors.white,
-                child: FlatButton(
-                    color: Colors.white,
-                    child: Text('RESET GRID', style: TextStyle(color: Colors.black)),
-                    onPressed: () => _clearArray(),
-                ),
-            ),
+          ),
         ),
         // Fill the rest up until the bottom of the screen.
         Expanded(child: Container(color: Colors.white)),
